daa04770f03007709784f5348b475f2af5cc5e4d
Musa Liman <musaliman27@gmail.com>
2023-02-12 11:35:30 +0100

    second  commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,14 +2,14 @@
 import React from 'react';
 import BootstrapCarousel from "../components/BootstrapCarousel";
 import IntroSection from '../components/IntroSection';
-import MissonSec from '../components/MissonSec';
+import MissionSec from '../components/MissionSec';
 import Testimonials from '../components/Testimonials';
 const Home = () => {
     return (
         <div>
             <BootstrapCarousel/>
 <IntroSection/>
-           <MissonSec/>
+           <MissionSec/>
            <Testimonials/>
         </div>
     );

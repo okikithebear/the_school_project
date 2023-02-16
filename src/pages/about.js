43704f3f6e99f051b5c34acec158f6f67d66a21b
obed <b.obed@yahoo.com>
2023-02-16 09:30:12 +0100

    Update: About page header

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const About = () => {
                 <div className='container'>
                     <div className='row'>
                         <div className='col-md-4 my-auto'>
-                        <h4 className="text-darkBlue text-3xl font-extrabold font-mulish">About Us.</h4>
+                            <h4 className="text-darkBlue text-3xl font-extrabold font-mulish">About Us.</h4>
                         </div>
                     </div>
                 </div>
@@ -18,22 +18,24 @@ const About = () => {
                     <h5 className='main-heading'>School Name</h5>
                     <div className='underline'></div>
                     <p>
-                    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
                         Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
                         quaerat impedit. Perferendis, maxime omnis."
                     </p>
                 </div>
             </section>
-
-            <MissonSec/>
+            <div className="p-6">
+                <MissonSec/>
+            </div>
             <section className='section  bg-light border-bottom'>
                 <div className='container'>
                     <h5 className='main-heading'>Why School Name?</h5>
                     <div className='underline'></div>
                     <p>
-                    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
                         Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
-                        quaerat impedit. Perferendis, maxime omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
+                        quaerat impedit. Perferendis, maxime omnis. Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Ipsum atque explicabo maxime nostrum.
                         Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
                         quaerat impedit. Perferendis, maxime omnis.
                     </p>

c53a412fd88c90e1c98a09c99fbe76404b0f51ed
Musa Liman <musaliman27@gmail.com>
2023-02-22 16:15:37 +0100

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,35 @@ const About = () => {
                 </div>
             </section>
             <div className="p-6">
-                <MissonSec/>
+            <div className="flex flex-col md:flex-row lg:mb-0 mt-8 md:mt-24 font-mulish">
+            <div className="basis-1/2 space-y-10">
+                <h1
+                    className="text-5xl font-semibold text-center text-darkBlue"
+                >
+                    Light Workers Academy Principles
+                </h1>
+                <p
+                    className="max-w-md mx-auto text-lg text-center text-slate-500  lg:text-left lg:mt-0 lg:mx-0"
+                >
+                    "To have a stable economy, to have a stable democracy, and to have a modern government is not enough. We have to build new pillars of development. Education, science and technology, innovation and entrepreneurship, and more equality." Sebastian Pinera
+                </p>
+                {/* <div
+                    className="flex items-center justify-center w-full space-x-4 text-white"
+                >
+                    <a
+                        href="#"
+                        className="p-3 text-sm font-semibold bg-darkBlue1 hover:bg-darkBlue hover:text-darkgreenVariant rounded-xl shadow-sm md:text-base"
+                    >Apply Here </a>
+                </div> */}
+            </div>
+            <div className="basis-1/2 lg:mb-0">
+                <img
+                    src="https://images.pexels.com/photos/7750766/pexels-photo-7750766.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    alt=""
+                    className="relative lg:top-24 xl:top-0 overflow-x-visible rounded-2xl"
+                />
+            </div>
+        </div>
             </div>
             <section className='section  bg-light border-bottom'>
                 <div className='container'>
@@ -33,30 +61,71 @@ const About = () => {
                 </div>
             </section>
 
-            <section>
-      <div
-        className="relative container flex flex-col max-w-6xl mx-auto my-56 px-6 text-gray-900 md:flex-row md:p-0"
-      >
-        <img src="https://images.pexels.com/photos/301926/pexels-photo-301926.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-        <div
-          className="top-24 pr-0 bg-white md:absolute md:right-0 md:py-20 md:pl-20"
-        >
-          <h2
-            className="max-w-lg mt-10 mb-6 font-sans text-4xl text-center text-darkBlue1 uppercase md:text-5xl md:mt-0 md:text-left"
-          >
-            The leader in interactive Teaching
-          </h2>
-          <p className="max-w-md  text-center md:text-left">
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
-            Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
-            quaerat impedit. Perferendis, maxime omnis. Lorem ipsum dolor, sit amet consectetur adipisicing
-            elit. Ipsum atque explicabo maxime nostrum.
-            Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
-            quaerat impedit. Perferendis, maxime omnis.
-          </p>
-          </div>
-      </div>
-    </section>
+        <section className='section-info bg-c-light border-top'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-12 mb-4 text-center text-darkgreenVariant'>
+                    <h3 className='main-heading'>Vision, Mission and Values</h3>
+                    <div className='underline mx-auto'></div>
+                    </div>
+                    <div className='col-md-4 text-justify font-mulish  text-darkgreenVariant'>
+                    <h6 className='font-semibold text-center'>Our Vision</h6>
+                        <p className='text-justify'>
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
+                        Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
+                        quaerat impedit. Perferendis, maxime omnis."  
+                        </p>
+                    </div>
+                    <div className='col-md-4 text-justify font-mulish text-darkgreenVariant'>
+                    <h6 className='font-semibold text-center'>Our Mission</h6>
+                        <p className='text-justify'>
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
+                        Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
+                        quaerat impedit. Perferendis, maxime omnis."  
+                        </p>
+                    </div>
+                    <div className='col-md-4  font-mulish text-darkgreenVariant'>
+                        <h6 className='font-semibold text-center'>Our Values</h6>
+                        <p className='text-justify'>
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
+                        Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
+                        quaerat impedit. Perferendis, maxime omnis."  
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className='section'>
+                    <h1 className='font-semibold text-center text-6xl  text-darkBlue1'>Founders</h1>
+                    <div className="container-fluid flex flex-col p-6 lg:flex-row lg:mb-0 font-mulish">
+     <div className="basis-1/2 lg:mx-0 lg:mb-0 lg:w-1/2">
+       <img
+         src="https://images.pexels.com/photos/7414209/pexels-photo-7414209.jpeg?auto=compress&cs=tinysrgb&w=600"
+         alt=""
+         className="relative  lg:top-24 xl:top-0 overflow-x-visible rounded-2xl"
+       />
+     </div>
+     <div className="basis-1/2 space-y-10 mt-16 lg:mt-16 lg:w-1/2 flex-wrap">
+            <h1 className="text-3xl font-semibold lg:text-5xl text-darkBlue">
+                Meet The people Behind The Dream
+            </h1>
+            <p className=" text-lg text-slate-500 w-5/6 lg:mt-0 lg:mx-0">
+            You can teach a student a lesson for a day; but if you can teach him to learn by creating curiosity, he will continue the learning process as long as he lives.”
+-Clay P. Bedford
+            </p>
+        </div>
+    </div>
+    <div className='container col-md-4 py-15 px-5 text-justify font-mulish  text-darkBlue1'>
+                    <h3 className='font-semibold text-2xl'>Babajide David and Lekan Aina ~</h3>
+                        <p>
+                        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
+                        Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
+                        quaerat impedit. Perferendis, maxime omnis."  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum atque explicabo maxime nostrum.
+                        Excepturi facere sapiente iste, sed porro praesentium, laudantium dolor aperiam veritatis optio
+                        quaerat impedit. Perferendis, maxime omnis." 
+                        </p>
+                    </div>
+                </section>
         </div>
     );
 };

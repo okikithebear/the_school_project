56540709ff356049896bdd6869ed33f4ef1c9211
Musa Liman <musaliman27@gmail.com>
2023-03-03 22:07:46 +0100

    school life update

--- src/pages/schoolLife.js
+++ src/pages/schoolLife.js
@@ -6,8 +6,8 @@ export default function Schoollife() {
     "https://images.pexels.com/photos/5212345/pexels-photo-5212345.jpeg?auto=compress&cs=tinysrgb&w=600",
     "https://images.pexels.com/photos/8471799/pexels-photo-8471799.jpeg?auto=compress&cs=tinysrgb&w=600",
     "https://images.pexels.com/photos/256455/pexels-photo-256455.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/5905497/pexels-photo-5905497.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/8617556/pexels-photo-8617556.jpeg?auto=compress&cs=tinysrgb&w=600",
+    // "https://images.pexels.com/photos/5905497/pexels-photo-5905497.jpeg?auto=compress&cs=tinysrgb&w=600",
+    // "https://images.pexels.com/photos/8617556/pexels-photo-8617556.jpeg?auto=compress&cs=tinysrgb&w=600",
   ];
 
   return (

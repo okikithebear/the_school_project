37e4f35937904584beada9cf5ac20d2a7a51277c
Musa Liman <musaliman27@gmail.com>
2023-03-25 09:55:06 +0100

    footer update

--- src/components/ItemsContainer.jsx
+++ src/components/ItemsContainer.jsx
@@ -2,7 +2,7 @@ import Item from "./Item";
 import { CONTACTS,QUICKLINKS } from "./Menus";
 const ItemsContainer = () => {
   return (
-    <div className="flex flex-row justify-between md:justify-center md:space-x-24 p-8">
+    <div className="flex flex-row justify-between md:justify-center md:space-x-24 md:space-y-0 p-8">
       <Item Links={CONTACTS} title="CONTACTS" />
       <Item Links={QUICKLINKS} title="QUICKLINKS" />
     </div>

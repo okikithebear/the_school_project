7b720fd30f2232cdb46b7123ef2fb3ae657a106c
obed <b.obed@yahoo.com>
2023-04-01 12:40:38 +0100

    Update: Footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,55 +36,56 @@ const Footer = () => {
   };
   return (
     <footer className="bg-darkBlue text-white font-mulish">
-      <div className="flex flex-col ">
-        <div className="flex flex-row  w-full">
-          <div className='flex flex-row my-12 py-6 w-2/3 justify-around'>
-            <div className='w-1/3'>
-              <h4 className="text-white font-bold mb-2">ABOUT US</h4>
-              <p className="text-gray-400 text-left ">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales neque ut ligula commodo, ac consequat nisi bibendum.
-              </p>
-              <form ref={form} onSubmit={sendEmail} >
-                <input
-                  name="user_email"
-                  type="text"
-                  placeholder="Enter Your Email here"
-                  className="text-gray-800 sm:w-72 w-full sm:mr-5 mr-1 h-10 rounded px-2 focus:outline-none my-6"
-                  required
-                />
-                <button value="Send" className=" p-2.5 text-sm font-semibold bg-darkgreenVariant hover:bg-greenVariant  hover:text-darkBlue1 rounded shadow-sm md:text-base">
-                  Subscribe  </button>
-                <ToastContainer theme="dark" />
-              </form>
+        <div className="flex flex-row w-full">
+          <div className='left-col flex flex-col relative pt-20 px-16 w-2/3'>
+            <div className='flex flex-row justify-evenly'>
+              <div className='subscription basis-1/3'>
+                <h4 className="text-white font-bold">SUBSCRIBE TO OUR NEWSLETTER</h4>
+                <p className="text-gray-400 text-left mt-6 ">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales neque ut ligula commodo, ac consequat nisi bibendum.
+                </p>
+                <form ref={form} onSubmit={sendEmail} >
+                  <input
+                    name="user_email"
+                    type="text"
+                    placeholder="Enter Your Email here"
+                    className="text-gray-800 sm:w-72 w-full sm:mr-5 mr-1 h-10 rounded px-2 focus:outline-none my-6"
+                    required
+                  />
+                  <button value="Send" className=" p-2.5 text-sm font-semibold bg-darkgreenVariant hover:bg-greenVariant
+                   hover:text-darkBlue1 rounded shadow-sm md:text-base">
+                    Subscribe  </button>
+                  <ToastContainer theme="dark" />
+                </form>
+              </div>
+              <div className="quick-links flex justify-end basis-1/3">
+                <ItemsContainer />
+              </div>
             </div>
-            <div className=" ">
-              <ItemsContainer />
+            <div className="socials flex flex-col my-3 items-center">
+              <h4 className="">&copy; 2023 All rights reserved.</h4>
+              <br />
+              <SocialIcons Icons={Icons} />
             </div>
           </div>
-          <div className="flex flex-col bg-[#ffffff19] p-12  w-1/3">
-            <div className="justify-center items-center  font-mulish text-white flex flex-col space-y-12">
+          <div className="right-col flex flex-col pt-20 px-12 bg-[#ffffff19] w-1/3">
+            <span className={'font-mulish'}>GET IN TOUCH</span>
+            <div className="justify-center mt-2 font-mulish text-white flex flex-col space-y-6">
               <h4 className=''>
                 <i className="fas fa-map-marker-alt mr-2 py-2 text-darkgreenVariant"></i>
-                <span className="text-darkgreenVariant font-extrabold text-lg">Address</span>: No1 alabokun street, Bodija.
+                <span className="text-darkgreenVariant font-extrabold mr-1.5">Address:</span> No1 alabokun street, Bodija.
               </h4>
               <h4 className=''>
                 <i className="fas fa-phone-alt mr-2 py-2 text-darkgreenVariant"></i>
-                <span className="text-darkgreenVariant font-extrabold text-lg">Telephone number</span>: 090887666776
+                <span className="text-darkgreenVariant font-extrabold mr-1.5">Telephone number:</span> +23490887666776
               </h4>
               <h4 className=''>
                 <i className="fas fa-envelope mr-2 py-2  text-darkgreenVariant"></i>
-                <span className="text-darkgreenVariant font-extrabold text-lg">Email</span>: lightworkers.ng.01@gmail.com
+                <span className="text-darkgreenVariant font-extrabold mr-1.5">Email:</span>lightworkers.ng.01@gmail.com
               </h4>
             </div>
           </div>
         </div>
-        <div className="bg-white flex flex-row py-2 px-6  items-center justify-center">
-          <h4 className="text-darkBlue1">&copy; 2023 All rights reserved.</h4>
-          <br />
-          <SocialIcons Icons={Icons} />
-        </div>
-      </div>
-
     </footer>
 
 
--- src/components/ItemsContainer.jsx
+++ src/components/ItemsContainer.jsx
@@ -2,8 +2,8 @@ import Item from "./Item";
 import { CONTACTS, QUICKLINKS } from "./Menus";
 const ItemsContainer = () => {
   return (
-    <div className="flex flex-row justify-between md:justify-center md:space-x-24 md:space-y-0 ">
-      <Item Links={CONTACTS} title="CONTACTS" />
+    <div className="flex flex-row md:space-y-0 ">
+      {/*<Item Links={CONTACTS} title="CONTACTS" />*/}
 
       <Item Links={QUICKLINKS} title="QUICKLINKS" />
     </div>
